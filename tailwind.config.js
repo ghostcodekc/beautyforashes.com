e62910e7a2f3ffd7294f445c047dc538bf702ca1
Andrew Grube <andrewmgrube@gmail.com>
2024-09-18 08:43:53 -0500

    Changed "pricing" language to "services"

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,7 +34,7 @@ module.exports = {
       },
       boxShadow: {
         input: "0px 7px 20px rgba(0, 0, 0, 0.03)",
-        pricing: "0px 39px 23px -27px rgba(0, 0, 0, 0.04)",
+        services: "0px 39px 23px -27px rgba(0, 0, 0, 0.04)",
         "switch-1": "0px 0px 5px rgba(0, 0, 0, 0.15)",
         testimonial: "0px 60px 120px -20px #EBEFFD",
       },
